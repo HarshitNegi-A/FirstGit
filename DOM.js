b340fd276cbe42a97082493a0b5a6f918668673c
HarshitNegi-A <harshitnegi226@gmail.com>
2024-09-18 09:06:30 +0530

    domManipulation

---
+++ DOM.js
@@ -0,0 +1,13 @@
+const header=document.getElementById('header')
+header.style.backgroundColor='green'
+header.style.borderBottom='3px solid orange'
+
+const mainHeading=document.getElementById('main-heading')
+mainHeading.textContent='Fruit World'
+mainHeading.style.color='orange'
+
+const h2Heading=document.getElementById('basket-heading')
+h2Heading.style.color='green'
+
+const thanks=document.getElementById('thanks')
+thanks.innerHTML='<p>Please visit us again</p>'
